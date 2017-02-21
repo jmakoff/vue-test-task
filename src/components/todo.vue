4d0725d6e5711ea799e7e1452d0ce1d1f698dd82
OneReach <antony.peklo@onereach.com>
2017-02-21 12:23:11 +0200

    improved "split tasks into tabs" task definition

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -28,7 +28,7 @@
                     {name : 'put "Add" button in one line with input', complete : false},
                     {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
                     {name : 'when task is complete cross it out', complete : false},
-                    {name : 'split tasks into "pending" and "complete" tabs with <ui-tabs>', complete : false},
+                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
                     {name : 'don\'t allow to add empty tasks', complete : false},
                     {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : false},
                     {name : 'extract list item into a separate vue.js component', complete : false},
