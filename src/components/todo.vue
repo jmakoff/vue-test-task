ca2449e64d8b0f2de3bb0bebad9354e592f33b9c
OneReach <antony.peklo@onereach.com>
2017-02-22 16:45:37 +0200

    update new task to start from small letter

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -26,7 +26,7 @@
                     {name : 'add ability to add tasks', complete : true},
                     {name : 'clear task name after clicking "Add"', complete : false},
                     {name : 'put "Add" button in one line with input', complete : false},
-                    {name : 'Add new task by hitting Enter instead of clicking "Add"', complete : false},
+                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : false},
                     {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
                     {name : 'when task is complete cross it out', complete : false},
                     {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
