53a7b7ccb837a3b2175e8e1d2611489bc699a681
Yuriy Makov <juramak98@gmail.com>
2017-04-21 14:59:18 +0300

    put "Add" button in one line with input

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,17 +1,32 @@
 <template>
     <div class="todo">
         <h1 class="title">Checklist</h1>
-        <ul class="tasks">
-            <li v-for="task in tasks" :class="{complete : task.complete}">
-                <label>
-                    <input type="checkbox" v-model="task.complete" />
-                    {{task.name}}
-                </label>
-            </li>
-        </ul>
+        <ui-tabs fullwidth="true" type="text">
+            <ui-tab class="tabHeight addScroll" title="pending" >
+
+
+                <ul class="tasks">
+                    <list-of-tasks></list-of-tasks>
+                    <!--<li v-for="task in tasks" v-if="task.complete ? false : true" :class="{complete : task.complete}">
+                        <ui-checkbox  v-model="task.complete" :label="task.name"/>
+                    </li>-->
+                </ul>
+
+
+            </ui-tab>
+            <ui-tab class="tabHeight addScroll" title="complete" >
+                <ul class="tasks">
+                    <li v-for="task in tasks" v-if="task.complete ? true : false" :class="{complete : task.complete}">
+                        <ui-checkbox v-model="task.complete" :label="task.name"/>
+                    </li>
+                </ul>
+
+            </ui-tab>
+        </ui-tabs>
         <div class="input_row">
-            <ui-textbox class= 'width_input' placeholder="e.g. 'read vue.js guide'" v-model="newTaskName"></ui-textbox>
-            <ui-button color="primary" @click="addTask" icon="add">Add</ui-button>
+            <ui-textbox class='width_input' placeholder="e.g. 'read vue.js guide'" v-model="newTaskName"
+                        @keydown-enter="addTask"></ui-textbox>
+            <ui-button color="primary" @click="addTask" icon="add" :disabled= "(newTaskName=='')? true : false" >Add</ui-button>
         </div>
     </div>
 </template>
@@ -20,28 +35,36 @@
     export default {
         data () {
             return {
-                newTaskName : '',
-                tasks : [
-                    {name : 'create skeleton of todo', complete : true},
-                    {name : 'add ability to add tasks', complete : true},
-                    {name : 'clear task name after clicking "Add"', complete : false},
-                    {name : 'put "Add" button in one line with input', complete : false},
-                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : false},
-                    {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
-                    {name : 'when task is complete cross it out', complete : false},
-                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
-                    {name : 'don\'t allow to add empty tasks', complete : false},
-                    {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : false},
-                    {name : 'extract list item into a separate vue.js component', complete : false},
-                    {name : 'persist tasks list in a local storage', complete : false},
-                    {name : 'add animation on task completion', complete : false},
+                newTaskName: '',
+                tasks: [
+                    {name: 'create skeleton of todo', complete: true},
+                    {name: 'add ability to add tasks', complete: true},
+                    {name: 'clear task name after clicking "Add"', complete: false},
+                    {name: 'put "Add" button in one line with input', complete: false},
+                    {name: 'add new task by hitting Enter instead of clicking "Add"', complete: false},
+                    {name: 'replace <input> with <ui-checkbox> in tasks list', complete: false},
+                    {name: 'when task is complete cross it out', complete: false},
+                    {
+                        name: 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>',
+                        complete: false
+                    },
+                    {name: 'don\'t allow to add empty tasks', complete: false},
+                    {name: 'make list of tasks scrollable, if there\'re are a lot of tasks', complete: false},
+                    {name: 'extract list item into a separate vue.js component', complete: false},
+                    {name: 'persist tasks list in a local storage', complete: false},
+                    {name: 'add animation on task completion', complete: false},
                 ]
             }
         },
 
-        methods : {
+        methods: {
+            replaceItem(){
+                setTimeout(function () {
+                    return false;
+                }, 1000)
+            },
             addTask () {
-                this.tasks.push({name : this.newTaskName, complete : false});
+                this.tasks.push({name: this.newTaskName, complete: false});
                 this.newTaskName = "";
             }
         }
@@ -49,30 +72,43 @@
 </script>
 
 <style scoped lang="scss">
+
     .todo {
         margin: auto;
         background: #fff;
         padding: 20px;
         border-radius: 5px;
         box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 15px;
+        width:60%;
+        height: 800px;
 
-        .title {
-            margin-top: 0;
-        }
+    .addScroll{
+        overflow-y: auto;
+    }
+    .tabHeight{
+        height:500px ;
+    }
 
-        .tasks {
-            list-style: none;
-            padding: 0;
-        }
-        .complete{
-            text-decoration: line-through;
-        }
-        .input_row{
-            .width_input{
-                width: 80%;
-                display: inline-flex;
-            }
+    .title {
+        margin-top: 0;
+    }
 
-        }
+    .tasks {
+        list-style: none;
+        padding: 0;
+    }
+
+    .complete {
+        text-decoration: line-through;
+    }
+
+    .input_row {
+
+    .width_input {
+        width: 91%;
+        display: inline-flex;
+    }
+
+    }
     }
 </style>
